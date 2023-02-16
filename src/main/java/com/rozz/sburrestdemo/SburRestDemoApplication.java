06ad166f14485634937095f4af832a67d386fd2e
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-02-16 11:08:56 +0000

    initial commit

---
+++ src/main/java/com/rozz/sburrestdemo/SburRestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rozz.sburrestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SburRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SburRestDemoApplication.class, args);
+	}
+
+}

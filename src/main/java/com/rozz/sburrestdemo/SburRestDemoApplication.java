b11e5763bc9a1e687ddbf7d9e5d1546a2e3f634a
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-02-16 13:22:12 +0000

    post coffee

--- src/main/java/com/rozz/sburrestdemo/SburRestDemoApplication.java
+++ src/main/java/com/rozz/sburrestdemo/SburRestDemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -71,5 +73,11 @@ class RestApiDemoController {
 			}
 		}
 		return Optional.empty();
+	}
+
+	@PostMapping("/coffees")
+	Coffee postCoffee(@RequestBody Coffee coffee) {
+		coffees.add(coffee);
+		return coffee;
 	}
 }

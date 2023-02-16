90a9b551454963d26bad0460c814bda8b18f4079
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-02-16 13:46:34 +0000

    Add status through ResponseEntity

--- src/main/java/com/rozz/sburrestdemo/SburRestDemoApplication.java
+++ src/main/java/com/rozz/sburrestdemo/SburRestDemoApplication.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -84,7 +86,7 @@ class RestApiDemoController {
 	}
 
 	@PutMapping("/{id}")
-	Coffee putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
+	ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
 		int coffeeIndex = -1;
 
 		for (Coffee c : coffees) {
@@ -93,7 +95,8 @@ class RestApiDemoController {
 				coffees.set(coffeeIndex, coffee);
 			}
 		}
-		return (coffeeIndex == -1) ? postCoffee(coffee) : coffee;
+		return (coffeeIndex == -1) ? new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED)
+				: new ResponseEntity<>(coffee, HttpStatus.OK);
 	}
 
 	@DeleteMapping("/{id}")

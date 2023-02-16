d996f6ef7fe97bb5fb7fcf3add092422095b705d
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-02-16 12:35:10 +0000

    add Coffee class

--- src/main/java/com/rozz/sburrestdemo/SburRestDemoApplication.java
+++ src/main/java/com/rozz/sburrestdemo/SburRestDemoApplication.java
@@ -1,5 +1,7 @@
 package com.rozz.sburrestdemo;
 
+import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +12,30 @@ public class SburRestDemoApplication {
 		SpringApplication.run(SburRestDemoApplication.class, args);
 	}
 
+}
+
+class Coffee {
+	private final String id;
+	private String name;
+
+	public Coffee(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Coffee(String name) {
+		this(UUID.randomUUID().toString(), name);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }

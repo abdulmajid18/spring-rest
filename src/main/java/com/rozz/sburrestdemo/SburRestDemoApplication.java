3b21acb1372e039625becbbc297b247e25355235
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-02-16 13:48:49 +0000

    remove redundant import

--- src/main/java/com/rozz/sburrestdemo/SburRestDemoApplication.java
+++ src/main/java/com/rozz/sburrestdemo/SburRestDemoApplication.java
@@ -6,15 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class SburRestDemoApplication {
